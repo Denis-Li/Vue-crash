95fccf38f55b21d485070fc19ca3d26130350714
Denis-Li <den.le.genplan@gmail.com>
2021-05-17 12:12:33 +0200

    work on todos 01

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque suscipit dolore recusandae voluptas soluta inventore, ducimus consectetur sunt ullam nisi.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
